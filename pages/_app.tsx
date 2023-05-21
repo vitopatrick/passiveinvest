6767fc28f6a16be5279808afe01d7d6d0507624d
theWarlock <fecypher@gmail.com>
2023-05-21 20:54:27 +0100

    added cookie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,23 @@ import UserProvider from "../context/UserContext";
 import { useChatSupport } from "../hooks/useChatSupport";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CookieConsent from "react-cookie-consent";
 
 export default function App({ Component, pageProps }: AppProps) {
   useChatSupport();
   return (
     <UserProvider>
       <Component {...pageProps} />
-
+      <CookieConsent
+        location="top"
+        buttonText="Ok Agree"
+        buttonClasses="bg-teal-500 p-3"
+        expires={150}
+        debug={true}
+      >
+        We collect cookies to analyze our website traffic and performance; we
+        never collect any personal data
+      </CookieConsent>
       <ToastContainer theme="colored" draggable closeOnClick />
     </UserProvider>
   );
