e86398aeb3ae8abc425499218fc073565afdb7fc
theWarlock <fecypher@gmail.com>
2023-06-02 13:25:20 +0100

    hide the message from the user

--- components/landing-page/hero/HeroText.tsx
+++ components/landing-page/hero/HeroText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroText = () => {
   return (
-    <div className="p-4 text-center bg-neutral-100">
+    <div className="hidden p-4 text-center bg-neutral-100">
       <p className="hidden md:block">
         CFDs are complex instruments and come with a high risk of losing money
         rapidly due to leverage. 79.04% of retail investor accounts lose money
